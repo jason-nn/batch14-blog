a7db85ace5d48044f8a90b4da337562ba923ed9c
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-29 10:33:18 +0800

    Add article controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
   validates :title, presence: true, uniqueness: true
-  validates :body, presence: true, uniqueness: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 5 }
 end
