b762cdee0fd2ccd89e506b051827d83451f73848
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-29 23:44:25 +0800

    Add delete comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,6 +15,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: 303
+  end
+
   private
 
   def comment_params
