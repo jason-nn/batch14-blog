f566af1f6ee7cdce628ca1effaf14382293449cd
Jason Ho <jason.ho@obf.ateneo.edu>
2022-01-31 03:23:40 +0800

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,39 +1,121 @@
-article_one =
-  Article.create(
-    title: 'Paul Graham on College',
+articles = [
+  {
+    title: 'College',
     body:
       "So it is with colleges. Colleges differ, but they're nothing like the stamp of destiny so many imagine them to be. People aren't what some admissions officer decides about them at seventeen. They're what they make themselves.\r\n\r\nIndeed, the great advantage of not caring where people went to college is not just that you can stop judging them (and yourself) by superficial measures, but that you can focus instead on what really matters. What matters is what you make of yourself.",
-  )
-
-article_two =
-  Article.create(
-    title:
-      'No one ever measures recruiters by the later performance of people they turn down.',
-    body:
-      "Actually, someone did, once. Mitch Kapor's wife Freada was in charge of HR at Lotus in the early years. (As he is at pains to point out, they did not become romantically involved till afterward.) At one point they worried Lotus was losing its startup edge and turning into a big company. So as an experiment she sent their recruiters the resumes of the first 40 employees, with identifying details changed. These were the people who had made Lotus into the star it was. Not one got an interview.",
-  )
-
-article_three =
-  Article.create(
-    title: 'Paul Graham on Informality',
+    comments: [
+      {
+        commenter: 'Paul',
+        body:
+          'And yet a lot of companies do. A lot of companies are very much influenced by where applicants went to college. How could they be? I think I know the answer to that.',
+      },
+      {
+        commenter: 'Paul',
+        body:
+          "There used to be a saying in the corporate world: \"No one ever got fired for buying IBM.\" You no longer hear this about IBM specifically, but the idea is very much alive; there is a whole category of \"enterprise\" software companies that exist to take advantage of it. People buying technology for large organizations don't care if they pay a fortune for mediocre software. It's not their money. They just want to buy from a supplier who seems safe—a company with an established name, confident salesmen, impressive offices, and software that conforms to all the current fashions. Not necessarily a company that will deliver so much as one that, if they do let you down, will still seem to have been a prudent choice. So companies have evolved to fill that niche.",
+      },
+      {
+        commenter: 'Paul',
+        body:
+          "I'm not saying, of course, that elite colleges have evolved to prey upon the weaknesses of large organizations the way enterprise software companies have. But they work as if they had. In addition to the power of the brand name, graduates of elite colleges have two critical qualities that plug right into the way large organizations work. They're good at doing what they're asked, since that's what it takes to please the adults who judge you at seventeen. And having been to an elite college makes them more confident.",
+      },
+    ],
+  },
+  {
+    title: 'Nerds and formality',
     body:
       "If you're a nerd, you can understand how important clothes are by asking yourself how you'd feel about a company that made you wear a suit and tie to work. The idea sounds horrible, doesn't it? In fact, horrible far out of proportion to the mere discomfort of wearing such clothes. A company that made programmers wear suits would have something deeply wrong with it.\r\n\r\nAnd what would be wrong would be that how one presented oneself counted more than the quality of one's ideas. That's the problem with formality. Dressing up is not so much bad in itself. The problem is the receptor it binds to: dressing up is inevitably a substitute for good ideas. It is no coincidence that technically inept business types are known as \"suits.\"\r\n\r\nNerds don't just happen to dress informally. They do it too consistently. Consciously or not, they dress informally as a prophylactic measure against stupidity.",
-  )
+    comments: [],
+  },
+  {
+    title: 'Good people',
+    body:
+      "What do I mean by good people? One of the best tricks I learned during our startup was a rule for deciding who to hire. Could you describe the person as an animal? It might be hard to translate that into another language, but I think everyone in the US knows what it means. It means someone who takes their work a little too seriously; someone who does what they do so well that they pass right through professional and cross over into obsessive.\r\n\r\nIf you think about people you know, you'll find the animal test is easy to apply. Call the person's image to mind and imagine the sentence \"so-and-so is an animal.\" If you laugh, they're not. You don't need or perhaps even want this quality in big companies, but you need it in a startup.",
+    comments: [],
+  },
+  {
+    title: 'Team size',
+    body:
+      "Ideally you want between two and four founders. It would be hard to start with just one. One person would find the moral weight of starting a company hard to bear. Even Bill Gates, who seems to be able to bear a good deal of moral weight, had to have a co-founder. But you don't want so many founders that the company starts to look like a group photo. Partly because you don't need a lot of people at first, but mainly because the more founders you have, the worse disagreements you'll have. When there are just two or three founders, you know you have to resolve disputes immediately or perish. If there are seven or eight, disagreements can linger and harden into factions. You don't want mere voting; you need unanimity.",
+    comments: [],
+  },
+  {
+    title: 'Technical founder',
+    body:
+      "In a technology startup, which most startups are, the founders should include technical people. During the Internet Bubble there were a number of startups founded by business people who then went looking for hackers to create their product for them. This doesn't work well.\r\n\r\nBusiness people are bad at deciding what to do with technology, because they don't know what the options are, or which kinds of problems are hard and which are easy.\r\n\r\nAnd when business people try to hire hackers, they can't tell which ones are good. Even other hackers have a hard time doing that. For business people it's roulette.",
+    comments: [],
+  },
+  {
+    title: 'MBA',
+    body:
+      "If you work your way down the Forbes 400 making an x next to the name of each person with an MBA, you'll learn something important about business school. After Warren Buffett, you don't hit another MBA till number 22, Phil Knight, the CEO of Nike. There are only 5 MBAs in the top 50.\r\n\r\nWhat you notice in the Forbes 400 are a lot of people with technical backgrounds. Bill Gates, Steve Jobs, Larry Ellison, Michael Dell, Jeff Bezos, Gordon Moore. The rulers of the technology business tend to come from technology, not business.\r\n\r\nSo if you want to invest two years in something that will help you succeed in business, the evidence suggests you'd do better to learn how to hack than get an MBA.",
+    comments: [
+      {
+        commenter: 'Paul',
+        body:
+          "Learning to hack is a lot cheaper than business school, because you can do it mostly on your own. For the price of a Linux box, a copy of K&R, and a few hours of advice from your neighbor's fifteen year old son, you'll be well on your way.",
+      },
+    ],
+  },
+  {
+    title: 'Growth hacking',
+    body:
+      "It's true that a restaurant with mediocre food can sometimes attract customers through gimmicks. But that approach is very risky. It's more straightforward just to make the food good.",
+    comments: [],
+  },
+  {
+    title: 'UX',
+    body:
+      "I learned something valuable from that. It's worth trying very, very hard to make technology easy to use.\r\n\r\nHackers are so used to computers that they have no idea how horrifying software seems to normal people. Stephen Hawking's editor told him that every equation he included in his book would cut sales in half.\r\n\r\nWhen you work on making technology easier to use, you're riding that curve up instead of down. A 10% improvement in ease of use doesn't just increase your sales 10%. It's more likely to double your sales.",
+    comments: [],
+  },
+  {
+    title: 'Pass / Fail',
+    body:
+      "Financially, a startup is like a pass / fail course. The way to get rich from a startup is to maximize the company's chances of succeeding, not to maximize the amount of stock you retain. So if you can trade stock for something that improves your odds, it's probably a smart move.",
+    comments: [],
+  },
+  {
+    title: 'Newscasters',
+    body:
+      "Sometimes the VCs want to install a new CEO of their own choosing. Usually the claim is that you need someone mature and experienced, with a business background. Maybe in some cases this is true.\r\n\r\nAnd yet Bill Gates was young and inexperienced and had no business background, and he seems to have done ok. Steve Jobs got booted out of his own company by someone mature and experienced, with a business background, who then proceeded to ruin the company.\r\n\r\nSo I think people who are mature and experienced, with a business background, may be overrated. We used to call these guys \"newscasters,\" because they had neat hair and spoke in deep, confident voices, and generally didn't know much more than they read on the teleprompter.",
+    comments: [],
+  },
+  {
+    title: 'Hiring',
+    body:
+      "If hiring unnecessary people is expensive and slows you down, why do nearly all companies do it? I think the main reason is that people like the idea of having a lot of people working for them. This weakness often extends right up to the CEO. If you ever end up running a company, you'll find the most common question people ask is how many employees you have. This is their way of weighing you. It's not just random people who ask this; even reporters do. And they're going to be a lot more impressed if the answer is a thousand than if it's ten.\r\n\r\nThis is ridiculous, really. If two companies have the same revenues, it's the one with fewer employees that's more impressive. When people used to ask me how many people our startup had, and I answered \"twenty,\" I could see them thinking that we didn't count for much. I used to want to add \"but our main competitor, whose ass we regularly kick, has a hundred and forty, so can we have credit for the larger of the two numbers?\"\r\n\r\nAs with office space, the number of your employees is a choice between seeming impressive, and being impressive. Any of you who were nerds in high school know about this choice. Keep doing it when you start a company.",
+    comments: [],
+  },
+  {
+    title: 'Experience',
+    body:
+      "It was the worst year of my adult life, but I learned, without realizing it at the time, a lot of valuable lessons about the software business. In this case they were mostly negative lessons:\r\n\r\n\r\n\r\ndon't have a lot of meetings; \r\n\r\ndon't have chunks of code that multiple people own; \r\n\r\ndon't have a sales guy running the company; \r\n\r\ndon't make a high-end product; \r\n\r\ndon't let your code get too big; \r\n\r\ndon't leave finding bugs to QA people; \r\n\r\ndon't go too long between releases; \r\n\r\ndon't isolate developers from users;",
+    comments: [
+      {
+        commenter: 'Paul',
+        body:
+          "But negative lessons are just as valuable as positive ones. Perhaps even more valuable: it's hard to repeat a brilliant performance, but it's straightforward to avoid errors.",
+      },
+    ],
+  },
+  {
+    title: 'Compressing',
+    body:
+      "I don't think the amount of bullshit you have to deal with in a startup is more than you'd endure in an ordinary working life. It's probably less, in fact; it just seems like a lot because it's compressed into a short period. So mainly what a startup buys you is time. That's the way to think about it if you're trying to decide whether to start one. If you're the sort of person who would like to solve the money problem once and for all instead of working for a salary for 40 years, then a startup makes sense.",
+    comments: [],
+  },
+]
 
-article_one.comments.create(
-  commenter: 'Paul',
-  body:
-    'And yet a lot of companies do. A lot of companies are very much influenced by where applicants went to college. How could they be? I think I know the answer to that.',
-)
-
-article_one.comments.create(
-  commenter: 'Paul',
-  body:
-    "There used to be a saying in the corporate world: \"No one ever got fired for buying IBM.\" You no longer hear this about IBM specifically, but the idea is very much alive; there is a whole category of \"enterprise\" software companies that exist to take advantage of it. People buying technology for large organizations don't care if they pay a fortune for mediocre software. It's not their money. They just want to buy from a supplier who seems safe—a company with an established name, confident salesmen, impressive offices, and software that conforms to all the current fashions. Not necessarily a company that will deliver so much as one that, if they do let you down, will still seem to have been a prudent choice. So companies have evolved to fill that niche.",
-)
-
-article_two.comments.create(
-  commenter: 'Paul',
-  body:
-    "I'm not saying, of course, that elite colleges have evolved to prey upon the weaknesses of large organizations the way enterprise software companies have. But they work as if they had. In addition to the power of the brand name, graduates of elite colleges have two critical qualities that plug right into the way large organizations work. They're good at doing what they're asked, since that's what it takes to please the adults who judge you at seventeen. And having been to an elite college makes them more confident.\r\n",
-)
+articles.each do |article|
+  Article.create(title: article[:title], body: article[:body])
+  reference = Article.last
+  if article[:comments].length > 0
+    article[:comments].each do |comment|
+      reference.comments.create(
+        commenter: comment[:commenter],
+        body: comment[:body],
+      )
+    end
+  end
+end
